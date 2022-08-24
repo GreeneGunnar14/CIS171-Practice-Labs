3cc5019cf6b69d2c7baaab8e8f0ee85fb5b12d1f
GreeneGunnar14 <69988106+GreeneGunnar14@users.noreply.github.com>
2022-08-24 16:49:32 -0500

    Began work on creating the task for PL1

---
+++ Practice_Lab_1.py
@@ -0,0 +1,9 @@
+#This is a practice lab. Complete the following task to the best of your ability and ask questions when you feel stuck.
+
+'''
+The task for this lab is to create a grid of 'playing cards' which the user can interact with to see their value.
+The cards should be represented by two lists:
+  The first list should be the backs of the cards, represented by '[]'
+  The second list should be the fronts of the cards and can be any single character
+The lists should be output in a grid to the user 
+'''
