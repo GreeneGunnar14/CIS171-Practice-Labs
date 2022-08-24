b8cff8b9cd83dbb28b1ae72671a6ff92a8503949
GreeneGunnar14 <69988106+GreeneGunnar14@users.noreply.github.com>
2022-08-24 15:56:46 -0500

    Created the file and added a description

---
+++ Practice_Lab_0.py
@@ -0,0 +1,13 @@
+#This is the first practice lab. Please complete the following tasks to th best of your ability and ask questions when you feel stuck
+
+'''
+Task 1
+Create a simple calculator that takes two numbers as input and outputs their sum
+'''
+
+#Your code goes here
+
+'''
+Task 2
+
+'''
