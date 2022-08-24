970a42eb8de6af92c03f52d4469fea3e7f80c2f1
GreeneGunnar14 <69988106+GreeneGunnar14@users.noreply.github.com>
2022-08-24 17:23:07 -0500

    Added tasks 1 through 4

--- Practice_Lab_1.py
+++ Practice_Lab_1.py
@@ -1,9 +1,49 @@
-#This is a practice lab. Complete the following task to the best of your ability and ask questions when you feel stuck.
+#This is a practice lab. Complete the following tasks to the best of your ability and ask questions when you feel stuck.
+
+#In this practice lab you will create a grid of 'playing cards' which the user can interact with through the terminal
 
 '''
-The task for this lab is to create a grid of 'playing cards' which the user can interact with to see their value.
-The cards should be represented by two lists:
-  The first list should be the backs of the cards, represented by '[]'
-  The second list should be the fronts of the cards and can be any single character
-The lists should be output in a grid to the user 
+Task 1
+Create a list called card_backs that contains 9 of these strings: '[]'
 '''
+
+#Your code goes here
+
+'''
+Task 2
+Create a list called card_fronts that contains 9 different single characters
+Make sure they are all string type variables
+'''
+  
+#Your code goes here
+
+#Do not delete or alter this for loop
+for _ in range(2):
+  
+  '''
+  Task 3
+  Use loops to display all nine card backs in a 3x3 grid
+  '''
+  
+  #Your code goes here
+  
+  '''
+  Task 4
+  Prompt the user to enter the row and column of the card they wish to turn over 
+  '''
+  
+  #Your code goes here
+  
+  '''
+  Task 5
+  
+  '''
+  
+  #Your code goes here
+  
+  '''
+  Task 6
+  
+  '''
+  
+  #Your code goes here
