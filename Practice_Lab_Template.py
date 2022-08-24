3cc70c2fae93be5fc55874c3abadbf47f14690c6
GreeneGunnar14 <69988106+GreeneGunnar14@users.noreply.github.com>
2022-08-24 15:58:46 -0500

    Created a basic template for practice labs

---
+++ Practice_Lab_Template.py
@@ -0,0 +1,36 @@
+#This is a practice lab. Complete the following tasks to the best of your ability and ask questions when you feel stuck.
+
+'''
+Task 1
+
+'''
+
+#Your code goes here
+
+'''
+Task 2
+
+'''
+
+#Your code goes here
+
+'''
+Task 3
+
+'''
+
+#Your code goes here
+
+'''
+Task 4
+
+'''
+
+#Your code goes here
+
+'''
+Task 5
+
+'''
+
+#Your code goes here
