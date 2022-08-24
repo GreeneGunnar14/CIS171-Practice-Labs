ace025d6323b191e8dd102f713f8aa80c490aa03
GreeneGunnar14 <69988106+GreeneGunnar14@users.noreply.github.com>
2022-08-24 16:43:33 -0500

    Added more tasks to PL0

--- Practice_Lab_0.py
+++ Practice_Lab_0.py
@@ -1,13 +1,47 @@
-#This is the first practice lab. Please complete the following tasks to th best of your ability and ask questions when you feel stuck
+#This is the first practice lab. Please complete the following tasks to the best of your ability and ask questions when you feel stuck.
 
 '''
 Task 1
-Create a simple calculator that takes two numbers as input and outputs their sum
+Create a simple calculator to take user input for two numbers and output their sum
 '''
 
 #Your code goes here
 
 '''
 Task 2
+Create program to get a user's name and then use it in a customized greeting
+'''
+
+#Your code goes here
 
 '''
+Task 3
+Using the list below, print out each element of the array on a separate line
+'''
+
+countdown = ['3', '2, '1, 'GO!!']
+#Your code goes here
+
+'''
+Task 4
+In the fewest lines of code possible, output a 4x5 (4 rows, 5 columns) grid displaying a character of your choice
+'''
+#Your code goes here
+
+'''
+Task 5
+At the top of this file, add the statement 'from random import randint'
+Get an integer from the user and use randint to multiply it by a random number from one to ten
+Output the user input, the random number, and the resulting product
+'''
+
+#Your code goes here
+
+'''
+Task 6
+Using the list below, create a grid with the same dimensions as the grid from task 4 but with a different character in each spot
+Hint: Use list indexing and loops to complete this in the fewest lines of code possible
+'''
+
+character_list = ['a', '!', 'y', '(', 'X', '*', '&', '9', '#', '^', 'J', 'U', 'k', '8', '?', '>', '<', '1', '%']
+#Your code goes here
